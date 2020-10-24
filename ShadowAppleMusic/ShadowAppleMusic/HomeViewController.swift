54e836b8bbc81b3c456bc9e4b0b9e8354d17c4fa
Moon HyoJae <followmoonrise@gmail.com>
2020-10-24 17:27:11 +0900

    ShadowAppleMusic : ShadowAppleMusic 프로젝트 생성, CollectionView 화면 구성

---
+++ ShadowAppleMusic/ShadowAppleMusic/HomeViewController.swift
@@ -0,0 +1,54 @@
+//
+//  HomeViewController.swift
+//  ShadowAppleMusic
+//
+//  Created by 문효재 on 2020/10/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
+extension HomeViewController: UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        switch kind {
+        case UICollectionView.elementKindSectionHeader:
+            return UICollectionReusableView()
+        default:
+            return UICollectionReusableView()
+        }
+    }
+    
+}
+
+
+extension HomeViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+}
+
+
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize.zero
+    }
+}
+
---
+++ ShadowAppleMusic/ShadowAppleMusic/TrackCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  TrackCollectionViewCell.swift
+//  ShadowAppleMusic
+//
+//  Created by 문효재 on 2020/10/24.
+//
+
+import UIKit
+
+class TrackCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var trackThumbnail: UIImageView!
+    @IBOutlet weak var trackTitle: UILabel!
+    @IBOutlet weak var trackArtist: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        trackThumbnail.layer.cornerRadius = 4
+        trackArtist.textColor = UIColor.systemGray2
+    }
+    
+}
