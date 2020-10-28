ea2aac51f856ce2a08664e56a27257373b8914c7
Moon HyoJae <followmoonrise@gmail.com>
2020-10-28 15:17:35 +0900

    feat(TodoList) : TodoList 기본 화면 구성

--- TodoList/TodoList/TodoListViewController.swift
+++ TodoList/TodoList/TodoListViewController.swift
@@ -9,21 +9,123 @@ import UIKit
 
 class TodoListViewController: UIViewController {
 
+//    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var inputViewBottom: NSLayoutConstraint!
+    @IBOutlet weak var inputTextField: UITextField!
+    
+    @IBOutlet weak var isTodayButton: UIButton!
+    @IBOutlet weak var addButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func isTodayButtonTapped(_ sender: Any){
+        
     }
-    */
+    
+    @IBAction func isTaskButtonTapped(_ sender: Any){
+        
+        
+    }
 
 }
+
+extension TodoListViewController {
+    @objc private func adjustInputView(noti: Notification){
+        guard let userInfo = noti.userInfo else { return  }
+    }
+}
+
+extension TodoListViewController: UICollectionViewDataSource{
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "todoCell", for: indexPath) as? TodoListCell else {
+            return UICollectionViewCell()
+        }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        switch kind {
+        case UICollectionView.elementKindSectionHeader:
+            guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "todoHeader", for: indexPath) as? TodoListHeader
+            else { return UICollectionReusableView() }
+            
+            return header
+        default:
+            return UICollectionReusableView()
+        }
+    }
+}
+
+extension TodoListViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        // TODO: 사이즈 계산하기
+        let width = collectionView.bounds.width
+        let height:CGFloat = 60
+        return CGSize(width: width, height: height)
+    }
+}
+
+class TodoListCell: UICollectionViewCell {
+    
+    @IBOutlet weak var checkButton: UIButton!
+    @IBOutlet weak var descriptionLabel : UILabel!
+    @IBOutlet weak var deleteButton: UIButton!
+    @IBOutlet weak var strikeThroughView: UIView!
+    
+    @IBOutlet weak var strikeThroughWidth: NSLayoutConstraint!
+    
+    var doneButtonTapHandler: ((Bool) -> Void)?
+    var deleteButtonTapHandler: (() -> Void)?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        reset()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        reset()
+    }
+    
+    func updateUI(todo: Todo){
+        
+    }
+    
+    private func showStrikeThrough(_ show: Bool){
+        
+    }
+    
+    private func reset(){
+        
+    }
+}
+
+
+
+class TodoListHeader:UICollectionReusableView {
+    @IBOutlet weak var subTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
+struct Todo {
+    let description: String
+    var bool: Bool
+}
+
