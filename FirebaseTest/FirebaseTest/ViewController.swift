93fd074934d442e30d664c8be4588904b32088d9
Moon HyoJae <followmoonrise@gmail.com>
2021-02-08 22:04:10 +0900

    feat : 데이터베이스 연동 스타터킷

--- FirebaseTest/FirebaseTest/ViewController.swift
+++ FirebaseTest/FirebaseTest/ViewController.swift
@@ -6,12 +6,28 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
+    @IBOutlet weak var dataLabel: UILabel!
+    
+    let db  = Database.database().reference()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+      updateLabel()
+    
+    }
+    
+    func updateLabel() {
+        db.child("firstData").observeSingleEvent(of: .value, with: { snapshot in
+            let value = snapshot.value as? String ?? ""
+            DispatchQueue.main.async {
+                self.dataLabel.text = value
+            }
+        })
+        
     }
 
 
