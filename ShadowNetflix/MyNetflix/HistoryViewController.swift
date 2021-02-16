d2566c635a64e7c2b3b5208992357d00e5e68c3b
Moon HyoJae <followmoonrise@gmail.com>
2021-02-16 19:50:59 +0900

    feat : 검색기록 TableView, Cell View 구성, Firebase에서 검색기록 가져오기

---
+++ ShadowNetflix/MyNetflix/HistoryViewController.swift
@@ -0,0 +1,46 @@
+//
+//  HistoryViewController.swift
+//  MyNetflix
+//
+//  Created by 문효재 on 2021/02/16.
+//  Copyright © 2021 com.joonwon. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class HistoryViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    let db = Database.database().reference().child("searchHistory")
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+
+        // Do any additional setup after loading the view.
+    }
+   
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        db.observeSingleEvent(of: .value){ (snapshot) in
+            print(snapshot.value)
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+class HistoryCell: UITableViewCell {
+    @IBOutlet weak var searchTerm: UILabel!
+}
