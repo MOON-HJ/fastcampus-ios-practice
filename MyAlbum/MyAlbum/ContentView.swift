016ad97f37a3e820d9c826c018810707bc6f11d7
Moon HyoJae <followmoonrise@gmail.com>
2021-04-04 18:59:36 +0900

    feat : (MyAlbum) UIKit to SwiftUI 스타터킷 구성

---
+++ MyAlbum/MyAlbum/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyAlbum
+//
+//  Created by 문효재 on 2021/04/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
