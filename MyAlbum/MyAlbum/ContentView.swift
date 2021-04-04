9f8ea2f47ced5808831b365d35056191a2365934
Moon HyoJae <followmoonrise@gmail.com>
2021-04-04 19:59:36 +0900

    feat : (MyAlbum) SwiftUI로 뷰 구성

--- MyAlbum/MyAlbum/ContentView.swift
+++ MyAlbum/MyAlbum/ContentView.swift
@@ -8,11 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var currentValue = 0
+    @State private var showingAlert = false
+    //    @State private var message = "가격은 ￦ \(currentValue) 입니다."
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        
+        GeometryReader{ geo in
+            VStack(alignment: .center){
+                Spacer()
+                
+                Text("My Pets")
+                    .baselineOffset(10.0)
+                    .font(.title)
+                
+                Spacer()
+                
+                Image("dog")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: geo.size.width * 0.8, height: geo.size.width * 0.8, alignment: .center)
+                    .clipped()
+                
+                
+                Spacer()
+                
+                Text("가격은 ￦ \(currentValue) 입니다.")
+                    .bold()
+                    .font(.title2)
+                    .frame(width:  geo.size.width, alignment: .center)
+                
+                Spacer()
+                
+                Button("refresh", action: {
+                    self.showingAlert = true
+                })
+                .font(.headline)
+                .foregroundColor(.pink)
+                
+                Spacer()
+                
+            }
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
